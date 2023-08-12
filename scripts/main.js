46730ddf6b57a10b81f6a53b71f1b3c0aba25619
johnnyji.dev <johnnyji.dev@gmail.com>
2023-08-12 07:33:35 +0000

    .

---
+++ scripts/main.js
@@ -0,0 +1,54 @@
+function changeDownSize() {
+
+    document.getElementsByClassName("face")[0].style.width = "100px";
+    document.getElementsByClassName("face")[1].style.width = "100px";
+    document.getElementsByClassName("face")[2].style.width = "100px";
+    document.getElementsByClassName("face")[3].style.width = "100px";
+    document.getElementsByClassName("face")[4].style.width = "100px";
+    document.getElementsByClassName("face")[5].style.width = "100px";
+
+    document.getElementsByClassName("face")[0].style.height = "100px";
+    document.getElementsByClassName("face")[1].style.height = "100px";
+    document.getElementsByClassName("face")[2].style.height = "100px";
+    document.getElementsByClassName("face")[3].style.height = "100px";
+    document.getElementsByClassName("face")[4].style.height = "100px";
+    document.getElementsByClassName("face")[5].style.height = "100px";
+
+    // alert('changeDownSize!');
+}
+
+function changeUpSize() {
+    document.getElementsByClassName("face")[0].style.width = "200px";
+    document.getElementsByClassName("face")[1].style.width = "200px";
+    document.getElementsByClassName("face")[2].style.width = "200px";
+    document.getElementsByClassName("face")[3].style.width = "200px";
+    document.getElementsByClassName("face")[4].style.width = "200px";
+    document.getElementsByClassName("face")[5].style.width = "200px";
+
+    document.getElementsByClassName("face")[0].style.height = "200px";
+    document.getElementsByClassName("face")[1].style.height = "200px";
+    document.getElementsByClassName("face")[2].style.height = "200px";
+    document.getElementsByClassName("face")[3].style.height = "200px";
+    document.getElementsByClassName("face")[4].style.height = "200px";
+    document.getElementsByClassName("face")[5].style.height = "200px";
+
+    // alert('changeUpSize!');
+}
+
+function changeTestSize() {
+
+    document.getElementsByClassName("face")[6].style.width = "100px";
+    document.getElementsByClassName("face")[7].style.width = "100px";
+    document.getElementsByClassName("face")[8].style.width = "100px";
+    document.getElementsByClassName("face")[9].style.width = "100px";
+    document.getElementsByClassName("face")[10].style.width = "100px";
+    document.getElementsByClassName("face")[11].style.width = "100px";
+
+    document.getElementsByClassName("face")[0].style.height = "100px";
+    document.getElementsByClassName("face")[1].style.height = "100px";
+    document.getElementsByClassName("face")[2].style.height = "100px";
+    document.getElementsByClassName("face")[3].style.height = "100px";
+    document.getElementsByClassName("face")[4].style.height = "100px";
+    document.getElementsByClassName("face")[5].style.height = "100px";
+
+}
